deff228f3f4d1ea1f9d5f13f72d1cef394afd741
Pruthviraj Sawant <sawantpruthvi3@gmail.com>
2025-08-06 19:07:33 +0000

    Learned abot datatypes in js

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JavaScript code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browse (it runs in browser as jS engine is present there)
+
+console.log(3 
+    +
+     3) //code readibility should be high
+
+console.log("pruthvi")
+
+//Data Types
+
+let name = "Pruthvi sawant"
+let age = 22
+let isLoggedIn = false
+let state; //undefined as no value
+
+//number => 2 to power 53 (or bigint use for large numbers)
+//string => ""
+//boolean => true/false
+//null => standalone value / representaion of empty value
+//undefined => value is not assigned
+//symbol = to identify uniqueness {unique}
+
+//object
+
+console.log(typeof "pruthvi") //typeof tell us the DataType of the vale in it.
+console.log(typeof age)
+
+console.log(typeof null) // type = object
+console.log(typeof undefined)// type = undefined
+
+
+
+
