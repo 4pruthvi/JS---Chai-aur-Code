1630ee2b54bf13f648c0b66d360bc6db54fd3e8f
Pruthviraj Sawant <sawantpruthvi3@gmail.com>
2025-09-05 06:21:49 +0000

    learned about scope and mini hostig

--- 03_basis/02_scopes.js
+++ 03_basis/02_scopes.js
@@ -15,3 +15,49 @@ if (true) {
 console.log(a);
 console.log(b);
 console.log(c);  //it is declared inside the scope but still it is accessed outsid soo var is not used
+
+//child functions can access the variables of the parent function
+
+function one() {
+    const username = 'hitesh'
+
+    function two() {
+        const website = 'youtube'
+        console.log(username);   
+    }
+    //console.log(website); //connot access the varible of child function
+    two()
+}
+
+one()
+
+if (true) {
+    const username = 'mahadev'
+    if (username === "mahadev") {
+        const website = ' google'
+        console.log(username + website);
+    }
+    //console.log(website); //error parent cannot acces variable of child class
+    
+}
+
+//console.log(username); //error will occur because username is definded insid scope 
+
+//+++++++++++++++++++++++++ intersting ++++++++++++++
+
+//it will run if we call the function befor or after decleration of function
+
+console.log(addOne(4))
+
+function addOne(num) {
+    return num + 1;
+}
+
+
+//function decelard inside a variable
+//this function will not run we it is called befor deceleration.
+
+const addTwo = function(num) {
+    return num + 2;
+}
+console.log(addTwo(8))
