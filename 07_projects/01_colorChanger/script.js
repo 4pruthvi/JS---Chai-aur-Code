fe52365fb515b5d6b89b9f935a531602d864597d
Pruthviraj Sawant <sawantpruthvi3@gmail.com>
2025-09-12 22:08:52 +0000

    Color changer project complicted

---
+++ 07_projects/01_colorChanger/script.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body')
+const button = document.querySelectorAll('.button')
+
+button.forEach(function(button) {
+  button.addEventListener('click',function(e) {
+    console.log(e.target)
+    if(e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id
+    }
+    else if(e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id
+    }
+    else if(e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id
+    }
+    else if(e.target.id === 'red') {
+      body.style.backgroundColor = e.target.id
+    }
+  })
+})
