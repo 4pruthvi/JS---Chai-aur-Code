2be3889b63b76f0ebe4b9a5c8b48b2b2447e3cac
Pruthviraj Sawant <sawantpruthvi3@gmail.com>
2025-09-13 09:22:36 +0000

    BMI Calculator project complicted

---
+++ 07_projects/02_BMICalculator/script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit',function(e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value)
+  const weight = parseInt(document.querySelector('#weight').value)
+  const results = document.querySelector('#results')
+
+  if(height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = (`Please enter a valid height ${height}`)
+  }
+  else if(weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = (`Please enter a valid weight ${weight}`)
+  }
+  else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show results
+    ////check the results whether it is under or over weight or normal
+    results.innerHTML = `<span>${bmi}</span>`
+
+    if (bmi < 18.6) {
+      results.innerHTML = (`You are under weight with bmi ${bmi}`)
+    }
+    if (bmi >= 18.6 && bmi < 24.9) {
+      results.innerHTML = (`You are weight is normal with bmi ${bmi}`)
+    }
+    if (bmi >= 24.9) {
+      results.innerHTML = (`You are over weight with bmi ${bmi}`)
+    }
+  }
+
+})
